68c83392aaf7cff60e662bdc88bafda8099ac7f6
TheMez <luuk55553@gmail.com>
2016-12-15 15:49:46 +0100

    Added basic Enemy

---
+++ Wink/GameObjects/Living/Enemy.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wink
+{
+    public class Enemy : Living
+    {
+        public Enemy(Level level, int layer) : base(level, layer, "Enemy")
+        {
+            level.Add(this);
+
+            TileField grid = level.Find("TileField") as TileField;
+            Tile ST = grid.Get(GameEnvironment.Random.Next(grid.Columns - 1), GameEnvironment.Random.Next(grid.Rows - 1)) as Tile;
+            if (!ST.Passable) while (!ST.Passable)
+            {
+                ST = grid.Get(GameEnvironment.Random.Next(grid.Columns - 1), GameEnvironment.Random.Next(grid.Rows - 1)) as Tile;
+            }
+            float tileX = (ST.TilePosition.ToVector2().X + 1) * ST.Height - ST.Height / 2;
+            float tileY = (ST.TilePosition.ToVector2().Y + 1) * ST.Width;
+            Position = new Vector2(tileX, tileY);
+            //InitAnimation(); not sure if overriden version gets played right without restating
+        }
+
+        protected override void InitAnimation()
+        {
+            base.InitAnimation();
+            PlayAnimation("idle");
+        }
+    }
+}
