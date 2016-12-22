d82b84f048476126db82a1bb825b91cbabdd5405
S-Schroevers <S.Schroevers@students.uu.nl>
2016-12-22 21:11:14 +0100

    close container when player moves out of range

--- Wink/GameObjects/InventorySystem/Container.cs
+++ Wink/GameObjects/InventorySystem/Container.cs
@@ -48,6 +48,21 @@ namespace Wink
         {
             base.Update(gameTime);
             iBox.Update(gameTime);
+
+            if (Visible)
+            {
+                //Not sure if it works with mul
+                string ClientName = Environment.MachineName;
+                Player player = GameWorld.Find("player_" + ClientName) as Player;
+
+                int dx = (int)Math.Abs(player.Position.X - player.Origin.X - Position.X);
+                int dy = (int)Math.Abs(player.Position.Y - player.Origin.Y - Position.Y);
+
+                if (!(dx <= Tile.TileWidth && dy <= Tile.TileHeight))
+                {
+                    iWindow.Visible = false;
+                }
+            }
         }
 
         public override void Draw(GameTime gameTime, SpriteBatch spriteBatch, Camera camera)
@@ -60,8 +75,8 @@ namespace Wink
         {
             Action onClick = () =>
             {
-                // correct player when in multiplayer?
-                Player player = GameWorld.Find(p => p is Player) as Player;
+                string ClientName = Environment.MachineName;
+                Player player = GameWorld.Find("player_" + Environment.MachineName) as Player;
 
                 int dx = (int)Math.Abs(player.Position.X - player.Origin.X - Position.X);
                 int dy = (int)Math.Abs(player.Position.Y - player.Origin.Y - Position.Y);
