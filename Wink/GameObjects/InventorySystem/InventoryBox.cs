018598330db556448e271b7b4dc344b2aaa63ae7
CallitMez <lhekkers054@gmail.com>
2017-01-10 12:26:42 +0100

    Default commit

--- Wink/GameObjects/InventorySystem/InventoryBox.cs
+++ Wink/GameObjects/InventorySystem/InventoryBox.cs
@@ -46,10 +46,10 @@ namespace Wink
             testItem4.ChangeItem(new BodyEquipment(20, 2, "empty:65:65:10:Brown"));
 
             ItemSlot testItem5 = itemGrid[2, 0] as ItemSlot;
-            testItem5.ChangeItem(new RingEquipment(50, RingType.health, "empty:65:65:10:DarkGray"));
+            testItem5.ChangeItem(new RingEquipment(500000, RingType.dexterity, "empty:65:65:10:DarkGray"));
 
             ItemSlot testItem6 = itemGrid[1, 0] as ItemSlot;
-            testItem6.ChangeItem(new RingEquipment(2, RingType.intelligence, "empty:65:65:10:DarkGray", true));
+            testItem6.ChangeItem(new RingEquipment(200, RingType.intelligence, "empty:65:65:10:DarkGray", true));
             // end test items
 
             Add(itemGrid);
