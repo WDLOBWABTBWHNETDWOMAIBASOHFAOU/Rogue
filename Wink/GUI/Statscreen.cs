c749ba455a99b669d332fea958aa68cb47cc37b8
David Kook <d.a.kook@students.uu.nl>
2017-01-26 14:35:19 +0100

    Points only visible when you have them

--- Wink/GUI/Statscreen.cs
+++ Wink/GUI/Statscreen.cs
@@ -13,21 +13,21 @@ namespace Wink
     {
         public StatScreen(Player player) : base(350, 300)
         {
-            
-           
+
+
             SpriteFont font = GameEnvironment.AssetManager.GetFont("Arial20");
-            int Point = 5;
+
             string Characterclassvalue = "Class";
 
             Color StatsColor = Color.Blue;
-            
+
             TextGameObject Character = new TextGameObject("Arial26", cameraSensitivity: 0, layer: 5, id: "Chacacter");
-            Character.Text = "Character Attributes  " ;
+            Character.Text = "Character Attributes  ";
             Character.Color = Color.Black;
             Add(Character);
 
             TextGameObject Characterclass = new TextGameObject("Arial20", cameraSensitivity: 0, layer: 5, id: "Chacacterclass");
-            Characterclass.Text = "Character Class   : " +Characterclassvalue; // later als
+            Characterclass.Text = "Character Class   : " + Characterclassvalue; // later als
             Characterclass.Color = Color.Black;
             Characterclass.Position = new Vector2(0, 315 - 9 * font.MeasureString(Characterclass.Text).Y);
             Add(Characterclass);
@@ -36,9 +36,9 @@ namespace Wink
 
 
             TextGameObject Strenght = new TextGameObject("Arial20", cameraSensitivity: 0, layer: 5, id: "Strenght");
-            Strenght.Text = "Strenght       "+ player.Strength;
+            Strenght.Text = "Strenght       " + player.Strength;
             Strenght.Color = StatsColor;
-            Strenght.Position = new Vector2(0, 300 - 7* font.MeasureString(Strenght.Text).Y);
+            Strenght.Position = new Vector2(0, 300 - 7 * font.MeasureString(Strenght.Text).Y);
             Add(Strenght);
 
             TextGameObject Dexterity = new TextGameObject("Arial20", cameraSensitivity: 0, layer: 5, id: "Dexterity");
@@ -62,7 +62,7 @@ namespace Wink
             TextGameObject Wisdom = new TextGameObject("Arial20", cameraSensitivity: 0, layer: 5, id: "Wisdom");
             Wisdom.Text = "Wisdom      " + player.Wisdom;
             Wisdom.Color = StatsColor;
-            Wisdom.Position = new Vector2(0, 300 - 3* font.MeasureString(Wisdom.Text).Y);
+            Wisdom.Position = new Vector2(0, 300 - 3 * font.MeasureString(Wisdom.Text).Y);
             Add(Wisdom);
 
             TextGameObject Luck = new TextGameObject("Arial20", cameraSensitivity: 0, layer: 5, id: "Luck");
@@ -71,32 +71,58 @@ namespace Wink
             Luck.Position = new Vector2(0, 300 - 2 * font.MeasureString(Luck.Text).Y);
             Add(Luck);
 
+
+            if (player.freeStatPoints>0)
+            { 
             TextGameObject PointsLeft = new TextGameObject("Arial20", cameraSensitivity: 0, layer: 5, id: "PointsLeft");
             PointsLeft.Text = "Points Left To Spend :" + player.freeStatPoints;
             PointsLeft.Color = Color.Black;
-            PointsLeft.Position = new Vector2(0 , 300 - font.MeasureString(PointsLeft.Text).Y);
+            PointsLeft.Position = new Vector2(0, 300 - font.MeasureString(PointsLeft.Text).Y);
             Add(PointsLeft);
 
-            //Left one in as  example, can't see to remove the bar, make it smaller or move it properly
-            
-            Button BLuck = new Button("inventory/slot", "  +", font, Color.Blue, 1,"", 0, 1);
-            BLuck.Position = new Vector2(0 + font.MeasureString(Luck.Text).X, 300- 2* font.MeasureString(Luck.Text).Y);
-            BLuck.Action = () =>
-            {
-                StatIncreaseEvent SIEL = new StatIncreaseEvent(player, Stat.luck);
-                Server.Send(SIEL);
-            };
-            Add(BLuck);
 
-            
-            Button BStrenght = new Button("inventory/slot", "  +", font, Color.Blue,1, "", 0, 1);
-            BStrenght.Position = new Vector2(0 + font.MeasureString(Strenght.Text).X, 300 - 7* font.MeasureString(Strenght.Text).Y);
+
+
+            Button BStrenght = new Button("inventory/slot", "  +", font, Color.Blue, 1, "", 0, 1);
+            BStrenght.Position = new Vector2(0 + font.MeasureString(Strenght.Text).X, 300 - 7 * font.MeasureString(Strenght.Text).Y);
             BStrenght.Action = () =>
             {
                 StatIncreaseEvent SIES = new StatIncreaseEvent(player, Stat.strength);
                 Server.Send(SIES);
             };
             Add(BStrenght);
+
+         
+
+            Button BDexterity = new Button("inventory/slot", "  +", font, Color.Blue, 1, "", 0, 1);
+            BDexterity.Position = new Vector2(0 + font.MeasureString(Dexterity.Text).X, 300 - 6 * font.MeasureString(Dexterity.Text).Y);
+            BDexterity.Action = () =>
+            {
+                StatIncreaseEvent SIED = new StatIncreaseEvent(player, Stat.dexterity);
+                Server.Send(SIED);
+            };
+            Add(BDexterity);
+
+
+            Button BIntelligence = new Button("inventory/slot", "  +", font, Color.Blue, 1, "", 0, 1);
+            BIntelligence.Position = new Vector2(0 + font.MeasureString(Intelligence.Text).X, 300 - 5 * font.MeasureString(Intelligence.Text).Y);
+            BIntelligence.Action = () =>
+            {
+                StatIncreaseEvent SIEI = new StatIncreaseEvent(player, Stat.intelligence);
+                Server.Send(SIEI);
+            };
+            Add(BIntelligence);
+
+
+            Button BVitality = new Button("inventory/slot", "  +", font, Color.Blue, 1, "", 0, 1);
+            BVitality.Position = new Vector2(0 + font.MeasureString(Vitality.Text).X, 300 - 4 * font.MeasureString(Vitality.Text).Y);
+            BVitality.Action = () =>
+            {
+                StatIncreaseEvent SIEV = new StatIncreaseEvent(player, Stat.vitality);
+                Server.Send(SIEV);
+            };
+            Add(BVitality);
+
 
             Button BWisdom = new Button("inventory/slot", "  +", font, Color.Blue, 1, "", 0, 1);
             BWisdom.Position = new Vector2(0 + font.MeasureString(Wisdom.Text).X, 300 - 3 * font.MeasureString(Wisdom.Text).Y);
@@ -108,32 +134,15 @@ namespace Wink
             Add(BWisdom);
 
 
-            Button BDexterity = new Button("inventory/slot", "  +", font, Color.Blue, 1, "", 0, 1);
-            BDexterity.Position = new Vector2(0 + font.MeasureString(Dexterity.Text).X, 300 - 6 * font.MeasureString(Dexterity.Text).Y);
-            BDexterity.Action = () =>
+            Button BLuck = new Button("inventory/slot", "  +", font, Color.Blue, 1, "", 0, 1);
+            BLuck.Position = new Vector2(0 + font.MeasureString(Luck.Text).X, 300 - 2 * font.MeasureString(Luck.Text).Y);
+            BLuck.Action = () =>
             {
-                StatIncreaseEvent SIED = new StatIncreaseEvent(player, Stat.dexterity);
-                Server.Send(SIED);
+                StatIncreaseEvent SIEL = new StatIncreaseEvent(player, Stat.luck);
+                Server.Send(SIEL);
             };
-            Add(BDexterity);
-
-            Button BIntelligence = new Button("inventory/slot", "  +", font, Color.Blue, 1, "", 0, 1);
-            BIntelligence.Position = new Vector2(0 + font.MeasureString(Intelligence.Text).X, 300 - 5 * font.MeasureString(Intelligence.Text).Y);
-            BIntelligence.Action = () =>
-            {
-                StatIncreaseEvent SIEI = new StatIncreaseEvent(player, Stat.intelligence);
-                Server.Send(SIEI);
-            };
-            Add(BIntelligence);
-
-            Button BVitality = new Button("inventory/slot", "  +", font, Color.Blue, 1, "", 0, 1);
-            BVitality.Position = new Vector2(0 + font.MeasureString(Vitality.Text).X, 300 - 4 * font.MeasureString(Vitality.Text).Y);
-            BVitality.Action = () =>
-            {
-                StatIncreaseEvent SIEV = new StatIncreaseEvent(player, Stat.vitality);
-                Server.Send(SIEV);
-            };
-            Add(BVitality);
+            Add(BLuck);
+        }
         }
 
     }
