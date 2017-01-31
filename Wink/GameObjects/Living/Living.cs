f602e187d0bea45e54f087a0155df0862996d7dc
David Kook <d.a.kook@students.uu.nl>
2017-02-01 00:23:36 +0100

    Added the shorted 2 step sound for moving 1 tile

--- Wink/GameObjects/Living/Living.cs
+++ Wink/GameObjects/Living/Living.cs
@@ -277,7 +277,7 @@ namespace Wink
             }
             else if (Visible)
             {   // Movement animation
-                LocalServer.SendToClients(new LivingMoveAnimationEvent(this, t, "Sounds/footsteps-4"));
+                LocalServer.SendToClients(new LivingMoveAnimationEvent(this, t, "Sounds/Footsteps 2 steps"));
             }
         }
 
